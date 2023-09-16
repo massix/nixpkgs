183a0a2d313d2ecb1dc536b6118823ecb67303b7
natsukium <tomoya.otabi@gmail.com>
2023-09-16 12:26:04 +0900

    python311Packages.python-lzo: reformat

--- pkgs/development/python-modules/python-lzo/default.nix
+++ pkgs/development/python-modules/python-lzo/default.nix
@@ -1,22 +1,32 @@
-{ lib, fetchPypi, buildPythonPackage, lzo, pytestCheckHook }:
+{ lib, fetchPypi, buildPythonPackage, lzo, pytestCheckHook, setuptools, wheel }:
 
 buildPythonPackage rec {
   pname = "python-lzo";
   version = "1.15";
+  format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-pXqqAMXDoFFd2fdCa6LPYBdn3BncAj2LmdShOwoye0k=";
+    hash = "sha256-pXqqAMXDoFFd2fdCa6LPYBdn3BncAj2LmdShOwoye0k=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
+
   buildInputs = [ lzo ];
-  propagatedBuildInputs = [ ];
+
   nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [
+    "lzo"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/jd-boyd/python-lzo";
     description = "Python bindings for the LZO data compression library";
-    license = licenses.gpl2;
+    license = licenses.gpl2Only;
     maintainers = [ maintainers.jbedo ];
   };
 }
